0f428b49acd74191bc0eaadbe42ee31ca206e7f4
Kenny Mann <kmann@etherpunk.com>
2022-10-13 05:51:38 -0500

    Migrated to BCrypt.Net Adjusted methods accordingly. Added documentation for CreateHash. Really hope I understood the examples properly to implement this. Please don't hate me.

--- EtherpunkBlazorWasm/Shared/AppSettings.cs
+++ EtherpunkBlazorWasm/Shared/AppSettings.cs
@@ -4,4 +4,5 @@ public class AppSettings
     public const string ValidAudience = "EpunkAud";
     public const string ValidIssuer = "EpunkIss";
     public const string SecretKey = "4185ac7a3ee64060a3e895562d509c52";
+    public const string PasswordSalt = "k87L/MF28Q673VKh8/cPi.";
 }

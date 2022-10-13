561199ce8ba4d1c7066d33bca8605ef037c3d1e5
Kenny Mann <kmann@etherpunk.com>
2022-10-13 06:13:21 -0500

    Formatting

--- EtherpunkBlazorWasm/Server/Data/EpunkDbContext.cs
+++ EtherpunkBlazorWasm/Server/Data/EpunkDbContext.cs
@@ -11,31 +11,30 @@ public class EpunkDbContext : DbContext
         builder.Entity<AppUserRole>().HasKey(nameof(AppUserRole.RoleId), nameof(AppUserRole.UserId));
 
         // Uncommon the following if you want to add an admin user to view WeatherForecase admin area.
-  //      AppUser adminUser = new AppUser()
-  //      {
-  //          CreatedOn = DateTime.UtcNow,
-  //          Email = "admin@admin.com",
-  //          Id = Guid.NewGuid(),
-  //          PasswordHash = UserDatabase.CreateHash("password")
-  //      };
+        //AppUser adminUser = new AppUser()
+        //{
+        //    CreatedOn = DateTime.UtcNow,
+        //    Email = "admin@admin.com",
+        //    Id = Guid.NewGuid(),
+        //    PasswordHash = UserDatabase.CreateHash("password")
+        //};
 
-  //      AppRole adminRole = new AppRole()
-  //      {
-  //          Id = Guid.NewGuid(),
-  //          Role = "Admin"
-  //      };
+        //AppRole adminRole = new AppRole()
+        //{
+        //    Id = Guid.NewGuid(),
+        //    Role = "Admin"
+        //};
 
-  //      AppUserRole adminUserRoleLink = new AppUserRole
-  //      {
-  //          RoleId = adminRole.Id,
-  //          UserId = adminUser.Id,
-  //      };
+        //AppUserRole adminUserRoleLink = new AppUserRole
+        //{
+        //    RoleId = adminRole.Id,
+        //    UserId = adminUser.Id,
+        //};
 
-  //      builder.Entity<AppUser>().HasData(adminUser);
-		//builder.Entity<AppRole>().HasData(adminRole);
-		//builder.Entity<AppUserRole>().HasData(adminUserRoleLink);
-
-	}
+        //builder.Entity<AppUser>().HasData(adminUser);
+        //builder.Entity<AppRole>().HasData(adminRole);
+        //builder.Entity<AppUserRole>().HasData(adminUserRoleLink);
+    }
 
 	protected override void OnConfiguring(DbContextOptionsBuilder options)
     {

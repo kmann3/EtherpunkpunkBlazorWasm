cc494df3d38b8cb6d446619c675301efb649f892
Kenny Mann <kmann@etherpunk.com>
2022-10-13 06:16:19 -0500

    Formatting.

--- EtherpunkBlazorWasm/Server/Data/EpunkDbContext.cs
+++ EtherpunkBlazorWasm/Server/Data/EpunkDbContext.cs
@@ -6,43 +6,42 @@ namespace EtherpunkBlazorWasm.Server.Data;
 
 public class EpunkDbContext : DbContext
 {
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        builder.Entity<AppUserRole>().HasKey(nameof(AppUserRole.RoleId), nameof(AppUserRole.UserId));
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder.Entity<AppUserRole>().HasKey(nameof(AppUserRole.RoleId), nameof(AppUserRole.UserId));
 
-        // Uncommon the following if you want to add an admin user to view WeatherForecase admin area.
-  //      AppUser adminUser = new AppUser()
-  //      {
-  //          CreatedOn = DateTime.UtcNow,
-  //          Email = "admin@admin.com",
-  //          Id = Guid.NewGuid(),
-  //          PasswordHash = UserDatabase.CreateHash("password")
-  //      };
+		// Uncommon the following if you want to add an admin user to view WeatherForecase admin area.
+		//AppUser adminUser = new AppUser()
+		//{
+		//    CreatedOn = DateTime.UtcNow,
+		//    Email = "admin@admin.com",
+		//    Id = Guid.NewGuid(),
+		//    PasswordHash = UserDatabase.CreateHash("password")
+		//};
 
-  //      AppRole adminRole = new AppRole()
-  //      {
-  //          Id = Guid.NewGuid(),
-  //          Role = "Admin"
-  //      };
+		//AppRole adminRole = new AppRole()
+		//{
+		//    Id = Guid.NewGuid(),
+		//    Role = "Admin"
+		//};
 
-  //      AppUserRole adminUserRoleLink = new AppUserRole
-  //      {
-  //          RoleId = adminRole.Id,
-  //          UserId = adminUser.Id,
-  //      };
+		//AppUserRole adminUserRoleLink = new AppUserRole
+		//{
+		//    RoleId = adminRole.Id,
+		//    UserId = adminUser.Id,
+		//};
 
-  //      builder.Entity<AppUser>().HasData(adminUser);
+		//builder.Entity<AppUser>().HasData(adminUser);
 		//builder.Entity<AppRole>().HasData(adminRole);
 		//builder.Entity<AppUserRole>().HasData(adminUserRoleLink);
-
 	}
 
 	protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        options.UseSqlite($"DataSource=EpunkSqliteDbName.sqlite3");
-    }
+	{
+		options.UseSqlite($"DataSource=EpunkSqliteDbName.sqlite3");
+	}
 
 	public DbSet<AppUserRole> AppUserRoles { get; set; }
 	public DbSet<AppUser> AppUsers { get; set; }
-    public DbSet<AppRole> AppRoles { get; set; }
+	public DbSet<AppRole> AppRoles { get; set; }
 }

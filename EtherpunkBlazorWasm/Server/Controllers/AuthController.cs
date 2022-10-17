b5286bd12b2933a1db26a8b733dcc89470ddfbc8
Kenny Mann <kmann@etherpunk.com>
2022-10-16 20:52:01 -0500

    Remove unnecessary using

--- EtherpunkBlazorWasm/Server/Controllers/AuthController.cs
+++ EtherpunkBlazorWasm/Server/Controllers/AuthController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
-using System.Runtime.CompilerServices;
 using System.Security.Claims;
 
 namespace EtherpunkBlazorWasm.Server.Controllers;

a408b7eb25c819ce04e3c42d4ca7392c60f6cddb
Kenny Mann <kmann@etherpunk.com>
2022-10-27 04:58:16 -0500

    Organizing Shared classes better. Add RenameRole class.

---
+++ EtherpunkBlazorWasm/Shared/Auth/RegistrationModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EtherpunkBlazorWasm.Shared;
+public class RegistrationModel : LoginModel
+{
+    [Required(ErrorMessage = "Confirm password is required.")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Password and confirm password do not match.")]
+    public string ConfirmPassword { get; set; }
+}
---
+++ EtherpunkBlazorWasm/Shared/Auth/RenameRole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtherpunkBlazorWasm.Shared;
+public class RenameRole
+{
+    public Guid RoleId { get; set; }
+    public string NewName { get; set; }
+}

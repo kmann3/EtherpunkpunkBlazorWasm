51d775fa0870178292e80e3ce1cec954470c77b3
Kenny Mann <kmann@etherpunk.com>
2022-10-27 04:58:36 -0500

    Removing old classes that were moved during rename. Why wasn't this in the last stage?

--- EtherpunkBlazorWasm/Shared/LoginResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EtherpunkBlazorWasm.Shared;
-public class LoginResult
-{
-    public string Message { get; set; }
-    public string Email { get; set; }
-    public string JwtBearer { get; set; }
-    public bool Success { get; set; }
-}

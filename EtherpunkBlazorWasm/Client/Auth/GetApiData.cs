7f31d3a828e44ba946429a5fe15de2a4469e1385
Kenny Mann <kmann@etherpunk.com>
2022-11-05 05:19:01 -0500

    I'm an idiot. Didn't assign response headers which was causing me to assume it was failing

--- EtherpunkBlazorWasm/Client/Auth/GetApiData.cs
+++ EtherpunkBlazorWasm/Client/Auth/GetApiData.cs
@@ -119,7 +119,7 @@ public static class ApiHandler
         try
         {
             var message = await http.PostAsJsonAsync<T1>(uri, sendData, CancellationToken.None);
-
+            returnData.HttpResponse = message;
             if (!message.IsSuccessStatusCode)
             {
                 returnData.ErrorData = new();
@@ -175,7 +175,7 @@ public static class ApiHandler
         try
         {
             var message = await http.PostAsJsonAsync<T1>(uri, sendData, CancellationToken.None);
-
+            returnData.HttpResponse = message;
             if (!message.IsSuccessStatusCode)
             {
                 returnData.ErrorData = new();
@@ -208,6 +208,121 @@ public static class ApiHandler
                             break;
                     }
                 }
+            }
+            if (message.IsSuccessStatusCode && message.Content != null)
+            {
+                returnData.Data = await message.Content?.ReadFromJsonAsync<T2?>();
+            }
+        }
+        catch (Exception ex)
+        {
+            // This will NOT catch errors in the API itself. Only exception thrown in this method.
+            returnData.ErrorData = new ReturnData<T2>.ErrorDetail()
+            {
+                Exception = ex
+            };
+        }
+
+        return returnData;
+    }
+    public static async Task<ReturnDataList<T2>> PutApiDataListAsync<T1, T2>(string uri, T1 sendData, IJSRuntime jsr, HttpClient http)
+    {
+        var returnData = new ReturnDataList<T2>();
+        try
+        {
+            var message = await http.PutAsJsonAsync<T1>(uri, sendData, CancellationToken.None);
+            returnData.HttpResponse = message;
+            if (!message.IsSuccessStatusCode)
+            {
+                returnData.ErrorData = new();
+
+                if (returnData.HttpResponse == null)
+                {
+                    returnData.ErrorData.Message = message.StatusCode.ToString() + ";";
+                    returnData.ErrorData.Message += message.ReasonPhrase + ";";
+                    returnData.ErrorData.Message += message.Content + ";";
+                }
+                else
+                {
+                    returnData.ErrorData.Message = message.StatusCode.ToString() + ";";
+                    switch (returnData.HttpResponse.StatusCode)
+                    {
+                        case System.Net.HttpStatusCode.Unauthorized:
+                            await jsr.InvokeVoidAsync("localStorage.removeItem", "user").ConfigureAwait(false);
+                            returnData.ErrorData.Message = "Unauthorized. Token expired or not logged in.";
+                            break;
+                        case System.Net.HttpStatusCode.Forbidden:
+                            returnData.ErrorData.Message = "Not allowed to see this!";
+                            break;
+                        case System.Net.HttpStatusCode.NoContent:
+                            break;
+                        case System.Net.HttpStatusCode.InternalServerError:
+                            returnData.ErrorData.Message = "Internal Server Error. Probably an exception was thrown in the API.";
+                            break;
+                        default:
+                            returnData.ErrorData.Message = $"Unknown error: {returnData.HttpResponse.StatusCode}";
+                            break;
+                    }
+                }
+            }
+            if (message.IsSuccessStatusCode && message.Content != null)
+            {
+                returnData.Data = await message.Content?.ReadFromJsonAsync<List<T2>?>();
+            }
+        }
+        catch (Exception ex)
+        {
+            // This will NOT catch errors in the API itself. Only exception thrown in this method.
+            returnData.ErrorData = new ReturnDataList<T2>.ErrorDetail()
+            {
+                Exception = ex
+            };
+        }
+
+        return returnData;
+    }
+    public static async Task<ReturnData<T2>> PutApiDataAsync<T1, T2>(string uri, T1 sendData, IJSRuntime jsr, HttpClient http)
+    {
+        var returnData = new ReturnData<T2>();
+        try
+        {
+            var message = await http.PutAsJsonAsync<T1>(uri, sendData, CancellationToken.None);
+            returnData.HttpResponse = message;
+            if (!message.IsSuccessStatusCode)
+            {
+                returnData.ErrorData = new();
+
+                if (returnData.HttpResponse == null)
+                {
+                    returnData.ErrorData.Message = message.StatusCode.ToString() + ";";
+                    returnData.ErrorData.Message += message.ReasonPhrase + ";";
+                    returnData.ErrorData.Message += message.Content + ";";
+                }
+                else
+                {
+                    returnData.ErrorData.Message = message.StatusCode.ToString() + ";";
+                    switch (returnData.HttpResponse.StatusCode)
+                    {
+                        case System.Net.HttpStatusCode.Unauthorized:
+                            await jsr.InvokeVoidAsync("localStorage.removeItem", "user").ConfigureAwait(false);
+                            returnData.ErrorData.Message = "Unauthorized. Token expired or not logged in.";
+                            break;
+                        case System.Net.HttpStatusCode.Forbidden:
+                            returnData.ErrorData.Message = "Not allowed to see this!";
+                            break;
+                        case System.Net.HttpStatusCode.NoContent:
+                            break;
+                        case System.Net.HttpStatusCode.InternalServerError:
+                            returnData.ErrorData.Message = "Internal Server Error. Probably an exception was thrown in the API.";
+                            break;
+                        default:
+                            returnData.ErrorData.Message = $"Unknown error: {returnData.HttpResponse.StatusCode}";
+                            break;
+                    }
+                }
+            }else
+            {
+                returnData.ErrorData = null;
             }
             if (message.IsSuccessStatusCode && message.Content != null)
             {
